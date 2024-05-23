eb8c0d3cb117a38caca2688e34746381c39955b8
Shawn Ling <shawn@beets3d.cn>
2024-05-23 15:56:48 +0800

    Add Region-selectable OCR-recognised text and save to clipboard

---
+++ openterface/ShotOrc/AreaSeletor.swift
@@ -0,0 +1,8 @@
+//
+//  AreaSeletor.swift
+//  openterface
+//
+//  Created by Shawn Ling on 2024/5/23.
+//
+
+import Foundation
---
+++ openterface/ShotOrc/SCContext.swift
@@ -0,0 +1,8 @@
+//
+//  SCContext.swift
+//  openterface
+//
+//  Created by Shawn Ling on 2024/5/23.
+//
+
+import Foundation
