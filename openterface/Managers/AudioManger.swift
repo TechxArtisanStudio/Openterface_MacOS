8e65448a70ec26638b91900390f5d9f2c83c4ad7
shawn <ling3ye@outlook.com>
2025-03-04 13:18:52 +0800

    Add Initial Manager Classes for Audio, Device Notification, and Video

---
+++ openterface/Managers/AudioManger.swift
@@ -0,0 +1,8 @@
+//
+//  AudioManger.swift
+//  openterface
+//
+//  Created by Shawn on 2025/3/4.
+//
+
+import Foundation
---
+++ openterface/Managers/VideoManeger.swift
@@ -0,0 +1,8 @@
+//
+//  VideoManeger.swift
+//  openterface
+//
+//  Created by Shawn on 2025/3/4.
+//
+
+import Foundation
